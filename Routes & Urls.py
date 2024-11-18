6faf96d74761fcd86d152aae9e4082c0cf9dbf22
rohitmj <rohit.maharjan619@gmail.com>
2024-11-18 11:45:00 +1100

    Add files via upload

---
+++ Routes & Urls.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Hello World!</h1>"
+
+@app.route('/hello')
+def hello():
+    return "Hello World"
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello {name}"
+
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1, number2):
+    return f'{number1} + {number2} = {number1 + number2}'
+
+@app.route('/handle_url_params')
+def handle_params():
+    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+        greeting = request.args.get('greeting')
+        name = request.args.get('name')
+        return f'{greeting}, {name}'
+    else:
+        return 'Some parameters are missing'
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5555, debug=True)
